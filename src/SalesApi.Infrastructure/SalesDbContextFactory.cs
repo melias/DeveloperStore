2bb68b79443db3d56bc6ab11a3928c183703d244
Maykon Elias <maykon@live.com>
2025-05-16 18:01:18 -0300

    feat: Adicionando migration e algumas mudanças necessarias para o db rodar limpo

---
+++ src/SalesApi.Infrastructure/SalesDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace SalesApi.Infrastructure;
+
+public class SalesDbContextFactory : IDesignTimeDbContextFactory<SalesDbContext>
+{
+    public SalesDbContext CreateDbContext(string[] args)
+    {
+        var basePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "../SalesApi"));
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<SalesDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new SalesDbContext(optionsBuilder.Options);
+    }
+}
